85ea40f176f7298bee76b8520a85d6bb922a503d
jimmy cruz <cruzperezjimmya@gmail.com>
2022-12-29 10:24:10 -0500

    reto completado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+// import { Component, NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+// // const routes: Routes = [
+// //   {path: '', component: AppComponent},
+// //   {path: '**', pathMatch: 'full', redirectTo:''}
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+// // ];
+
+// @NgModule({
+//   // imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
